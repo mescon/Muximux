d0d87938fd1cc950d5345afa1432cb393fca24e5
mescon <mescon@gmail.com>
2016-01-14 01:04:36 +0100

    Started a new fork with a bunch of updates.
    Renamed to MTPHP (Managethis PHP) to signify the difference and minimize
    confusion.
    
    Opted to do this outside of Managethis which is already good for novice
    users that might not want to fiddle with PHP.
    
    - Added logic for the generation of menu items and iframes.
    - Moved configuration to a separate config-file that will not be
    overwritten if you update from this repository.
    - Added ability to enable/disable items.
    
    To come:
    - Dynamically generated landing-pages without code-reuse.
    - Password protection of MTPHP
    
    Will add more functionality as I find the time and as long as it's fun.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Managethis
-A lightweight way to manage your HTPC.
+# MTPHP
+A lightweight way to manage your HTPC, dynamically handled with PHP and a simple configuration-file.
 
-This is based off of: https://codyhouse.co/gem/responsive-tabbed-navigation/
+This is a PHP enabled fork of (simpler/more lightweight) "Managethis" found here: https://github.com/Tenzinn3/Managethis
 
 This is a lightweight way to manage your HTPC apps without having to run anything extra, all you need is to have a webserver. It basically acts as a portal for all of your apps in once place so you don't need to keep multiple tabs open.
 
@@ -9,11 +9,13 @@ This is a lightweight way to manage your HTPC apps without having to run anythin
 
 ## Setup
 
-- To set it up clone and place the folder inside the root directory of your webserver then navigate to the landing folder. 
-- In your favourite editor open the html pages and add in the urls of your apps by replacing the "ENTER URL HERE" line.
+- To set it up clone and place the folder inside the root directory of your webserver then rename config.inc.php-example to config.inc.php
+- In your favourite editor open config.inc.php and read the instructions.
 - You can change the app icons by replacing them with ones from http://bootstrapdocs.com/v3.0.0/docs/components/ or http://fontawesome.io/icons/
-- Navigate to http://youripaddress/Managethis to access Managethis.
-- You can access your apps by clicking on the "Launch" button. This was implemented to stop you being hit by multiple login requests as soon as you start the app. It also speeds up loading time.
+- Navigate to http://youripaddress/mtphp to access MTPHP.
+- You can access your apps by clicking on the "Launch" button. This was implemented to stop you being hit by multiple login requests as soon as you start the app. It also speeds up loading time. You can enable or disable this functionality in the configuration file, for each app.
 - To reload an app, double click it in the menu. Only that specific page will reload.
+- You can enable or disable specific apps without having to remove them entirely by just editing the configuration file.
+- The configuration by default can not be read by a visitor, as long as you don't remove the top part of the file.
 
 You may want to setup htaccess to secure it but even if you don't your apps will not be accessible as long as they themselves are secure.
---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<?php
+$config = parse_ini_file('config.ini.php', true);
+
+function menuItems($config) {
+	if (empty($item)) $item = '';
+	foreach ($config as $keyname => $section) {
+		if(!empty($section["enabled"]) && !($section["enabled"]=="false") && ($section["enabled"]=="true")) {
+			if(!empty($section["default"]) && !($section["default"]=="false") && ($section["default"]=="true")) {
+				$item .= "<li><a data-content=\"" . $keyname . "\" href=\"#0\" class=\"selected\"><span class=\"". $section["icon"] ."\"></span> ". $section["name"] ."</a></li>\n";
+			} else {
+				$item .= "<li><a data-content=\"" . $keyname . "\" href=\"#0\"><span class=\"". $section["icon"] ."\"></span> ". $section["name"] ."</a></li>\n";
+			}
+		}
+	}
+	if (empty($item)) $item = '';
+	return $item;
+}
+
+
+function frameContent($config) {
+	if (empty($item)) $item = '';
+	foreach ($config as $keyname => $section) {
+		if(!empty($section["enabled"]) && !($section["enabled"]=="false") && ($section["enabled"]=="true")) {
+			if(!empty($section["default"]) && !($section["default"]=="false") && ($section["default"]=="true")) {
+				$item .= "<li data-content=\"". $keyname . "\" class=\"selected\"><iframe scrolling=\"auto\" src=\"". $section["url"] . "\" style=\"width:100%; height:926px\"></iframe></li>\n";
+			} else {
+				$item .= "<li data-content=\"". $keyname . "\"><iframe scrolling=\"auto\" src=\"". $section["url"] . "\" style=\"width:100%; height:926px\"></iframe></li>\n";
+			}
+		}
+	}
+	return $item;
+}
+
+?>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" /> <!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" /> <!--FontAwesome-->
+        
+	<link href='//fonts.googleapis.com/css?family=PT+Sans:400' rel='stylesheet' type='text/css'> <!-- Font -->
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+
+	<title>Managethis</title>
+</head>
+
+<body>
+<div class="cd-tabs">
+	<nav>
+		<ul class="cd-tabs-navigation">
+		<?php echo menuItems($config); ?>
+		</ul>
+	</nav>
+
+	<ul class="cd-tabs-content">
+		<?php echo frameContent($config); ?>
+	</ul>
+</div>
+
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
