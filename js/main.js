e2e21bc20cba8b3bd953b3d00bfb7be5eedf7d1c
Corey Jones <synik4l@gmail.com>
2016-01-19 12:57:33 -0600

    Added bunch of css. Rough add and remove buttons. and Ajax save placeholder

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
     var tabs = $('.cd-tabs');
 
-    tabs.each(function(){
+    tabs.each(function () {
         var tab = $(this),
             tabItems = tab.find('ul.cd-tabs-navigation, .main-nav'),
             tabContentWrapper = tab.children('ul.cd-tabs-content'),
             tabNavigation = tab.find('nav');
 
-        tabItems.on('click', 'a', function(event){
+        tabItems.on('click', 'a', function (event) {
             event.preventDefault();
             var selectedItem = $(this);
-            if( !selectedItem.hasClass('selected') ) {
+            if (!selectedItem.hasClass('selected')) {
                 var selectedTab = selectedItem.data('content'),
-                    selectedContent = tabContentWrapper.find('li[data-content="'+selectedTab+'"]'),
+                    selectedContent = tabContentWrapper.find('li[data-content="' + selectedTab + '"]'),
                     selectedContentHeight = selectedContent.innerHeight();
 
-                selectedItem.dblclick(function() {
+                selectedItem.dblclick(function () {
                     selectedContent.children('iframe').attr('src', selectedContent.children('iframe').attr('src'));
                 })
 
@@ -36,30 +36,29 @@ jQuery(document).ready(function($){
 
         //hide the .cd-tabs::after element when tabbed navigation has scrolled to the end (mobile version)
         checkScrolling(tabNavigation);
-        tabNavigation.on('scroll', function(){
+        tabNavigation.on('scroll', function () {
             checkScrolling($(this));
         });
     });
 
     // Keep hamburger-menu white when hovering items UNDER the hamburger menu.
-    $('.drop-nav').on('mouseover', function(){
+    $('.drop-nav').on('mouseover', function () {
         $('.main-nav a span:first').addClass('dd-active');
     })
 
-    $('.drop-nav').on('mouseout', function(){
+    $('.drop-nav').on('mouseout', function () {
         $('.main-nav a span:first').removeClass('dd-active');
     })
 
 
-
-    $('#reload').on('click', function(){
+    $('#reload').on('click', function () {
         var selectedFrame = $('.cd-tabs-content').find('.selected').children('iframe');
         selectedFrame.attr('src', selectedFrame.attr('src'));
     })
 
 
-    $(window).on('resize', function(){
-        tabs.each(function(){
+    $(window).on('resize', function () {
+        tabs.each(function () {
             var tab = $(this);
             checkScrolling(tab.find('nav'));
             tab.find('.cd-tabs-content').css('height', 'auto');
@@ -70,10 +69,10 @@ jQuery(document).ready(function($){
     });
 
 
-    function checkScrolling(tabs){
+    function checkScrolling(tabs) {
         var totalTabWidth = parseInt(tabs.children('.cd-tabs-navigation').width()),
             tabsViewport = parseInt(tabs.width());
-        if(tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
+        if (tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
             tabs.parent('.cd-tabs').addClass('is-ended');
         } else {
             tabs.parent('.cd-tabs').removeClass('is-ended');
@@ -81,50 +80,114 @@ jQuery(document).ready(function($){
     }
 
     // Measure viewport and subtract the height the navigation tabs, then resize the iframes.
-    function resizeIframe(){
+    function resizeIframe() {
         var newSize = $(window).height() - $('nav').height();
-        $('iframe').css({ 'height': newSize + 'px' });
+        $('iframe').css({'height': newSize + 'px'});
     }
 
     /*function hideDropdownMenu() {
-        if($(window).width() < 1024) {
-            $('.main-nav').hide();
-        } else {
-            $('.main-nav').show();
-        }
-    }*/
+     if($(window).width() < 1024) {
+     $('.main-nav').hide();
+     } else {
+     $('.main-nav').show();
+     }
+     }*/
 
-    $('.main-nav').hover(function (){
-                dropDownFixPosition($('.main-nav'),$('.drop-nav'));
-            });
+    $('.main-nav').hover(function () {
+        dropDownFixPosition($('.main-nav'), $('.drop-nav'));
+    });
 
-    function dropDownFixPosition(button,dropdown){
-          var dropDownTop = button.offset().top + button.outerHeight();
-            dropdown.css('top', dropDownTop + "px");
-            dropdown.css('left', $(window).width()-$('.drop-nav').width() - button.offset().left + "px");
+    function dropDownFixPosition(button, dropdown) {
+        var dropDownTop = button.offset().top + button.outerHeight();
+        dropdown.css('top', dropDownTop + "px");
+        dropdown.css('left', $(window).width() - $('.drop-nav').width() - button.offset().left + "px");
     }
 
-    function settings_addFalseCheckboxes(){
-        $('.applicationName').change(function(){
-            var section = $(this).attr('value');
-            $('.'+section+'-value').addClass('')
-            $('.'+section+'-value').removeClass('.'+section+'-value');
-            $('.'+section+'-value').removeAttribute("name")
-        });
+    function settings_Buttons() {
+        //Defaults
+        $('.applicationName').attr('disabled', 'disabled');
+        $('#sortable').sortable();
+        initButtonHandlers();
 
-        $('#settingsSubmit').click(function(event){
+
+        //Add new application button
+        $('#addApplication').button().click(function () {
+            $('#addApplication').attr('disabled','disabled');
+            $('#sortable').prepend(
+                '<li class="applicationContainer" id="newApplication"><div>Application: ' +
+                '<input class="applicationName" was="newApplication" type="text" value="">' +
+                '<input type="button" class="saveApp" value="Save"></div>' +
+                '<div>enabled:<input class="checkbox newApplication-value" name="newApplication-enabled" type="checkbox"></div>' +
+                '<div>default:<input class="checkbox newApplication-value" name="newApplication-default" type="checkbox"></div>' +
+                '<div>name:<input class="newApplication-value" name="newApplication-name" type="text" value=""></div>' +
+                '<div>url:<input class="newApplication-value" name="newApplication-url" type="text" value=""></div>' +
+                '<div>landingpage:<input class="checkbox newApplication-value" name="rutorrent-landingpage" type="checkbox" checked=""></div>' +
+                '<div>icon:<input class="newApplication-value" name="newApplication-icon" type="text" value="fa fa-globe"></div>' +
+                '<input type="button" class="saveButton" value="Save" id="save-newApplication">' +
+                '<input type="button" class="removeButton" value="Remove" id="remove-newApplication"></li>');
+            initButtonHandlers();
+        });
+    }
+
+    function initButtonHandlers(){
+        //Update Application Name Button Handler
+        $('.saveApp').button().click(function () {
+            $('#addApplication').removeAttr('disabled');
+            var applicationInput = $(this).prev();
+            if ($(this).attr('value') == "Update Application Name") {
+                applicationInput.removeAttr('disabled');
+                $(this).attr('value', 'Save');
+            }
+            else {
+                var section = $(this).prev().attr('was');
+                var newSection = $(this).prev().val();
+                applicationInput.attr('was', newSection);
+                $('.' + section + '-value').each(function () {
+                    var split = $(this).attr('name').split('-');
+                    $(this).removeAttr('name')
+                        .prop('name', newSection + "-" + split[1])
+                        .addClass(newSection + '-value')
+                        .removeClass(section + '-value');
+                });
+                $(this).attr('value', 'Update Application Name');
+                applicationInput.attr('disabled', 'disabled');
+                $(this).parents('li').attr('id',newSection)
+            }
+        });
+        //Remove Button Handler
+        $('.removeButton').button().click(function () {
+            runRemove($(this).parent('li'))
+        });
+        function runRemove(selectedElement) {
+            var selectedEffect = "drop"
+            var options = {};
+            $(selectedElement).effect(selectedEffect, options, 500, removeCallback(selectedElement));
+        };
+        function removeCallback(selectedElement) {
+            setTimeout(function () {
+                $(selectedElement).remove();
+            }, 1000);
+        };
+        //Save button handler - Add ajax
+        $('.saveButton').button();
+    }
+
+    function settings_addFalseCheckboxes() {
+        $('#settingsSubmit').click(function (event) {
             event.preventDefault();
-            $('.checkbox').each(function(){
-                if(!$(this).prop('checked')){
-                    var name=$(this).attr('name');
-                    $('form').append('<input type="hidden" name="'+name+'" value="false">');
+            $('.checkbox').each(function () {
+                if (!$(this).prop('checked')) {
+                    var name = $(this).attr('name');
+                    $('form').append('<input type="hidden" name="' + name + '" value="false">');
                 }
             });
             $('form').submit();
         });
 
     }
-settings_addFalseCheckboxes();
-resizeIframe(); // Call resizeIframe when document is ready
+
+    settings_addFalseCheckboxes();
+    settings_Buttons();
+    resizeIframe(); // Call resizeIframe when document is ready
 //hideDropdownMenu(); // Check if we should hide the dropdown menu
 });
