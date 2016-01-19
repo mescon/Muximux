74879fd36b2f4dea0787e84f1f3596a431c5561e
Corey Jones <synik4l@gmail.com>
2016-01-19 08:36:46 -0600

    Added config lite. Wrote settings.php load and save. Working on javascript still

---
+++ vendor/pear-pear.php.net/Config_Lite/Config/Lite/Exception/InvalidArgument.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Config_Lite_Exception_InvalidArgument (Config/Lite/Exception/InvalidArgument.php)
+ *
+ * PHP version 5
+ *
+ * @file      Config/Lite/Exception/InvalidArgument.php
+ * @category  Configuration
+ * @package   Config_Lite
+ * @author    Patrick C. Engel <pce@php.net>
+ * @copyright 2010-2011 <pce@php.net>
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version   SVN: $Id$
+ * @link      https://github.com/pce/config_lite
+ */
+
+
+/**
+ * Config_Lite_Exception_InvalidArgument
+ *
+ * implements Config_Lite_Exception
+ *
+ * @category  Configuration
+ * @package   Config_Lite
+ * @author    Patrick C. Engel <pce@php.net>
+ * @copyright 2010-2011 <pce@php.net>
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version   Release: 0.2.4
+ * @link      https://github.com/pce/config_lite
+ */
+class Config_Lite_Exception_InvalidArgument
+              extends RuntimeException
+              implements Config_Lite_Exception
+{
+}
---
+++ vendor/pear-pear.php.net/Config_Lite/Config/Lite/Exception/Runtime.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Config_Lite_Exception_Runtime (Config/Lite/Exception/Runtime.php)
+ *
+ * PHP version 5
+ *
+ * @file      Config/Lite/Exception/Runtime.php
+ * @category  Configuration
+ * @package   Config_Lite
+ * @author    Patrick C. Engel <pce@php.net>
+ * @copyright 2010-2011 <pce@php.net>
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version   SVN: $Id$
+ * @link      https://github.com/pce/config_lite
+ */
+
+
+/**
+ * Config_Lite_Exception_Runtime
+ *
+ * implements Config_Lite_Exception
+ *
+ * @category  Configuration
+ * @package   Config_Lite
+ * @author    Patrick C. Engel <pce@php.net>
+ * @copyright 2010-2011 <pce@php.net>
+ * @license   http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+ * @version   Release: 0.2.4
+ * @link      https://github.com/pce/config_lite
+ */
+
+class Config_Lite_Exception_Runtime
+              extends RuntimeException
+              implements Config_Lite_Exception
+{
+}
