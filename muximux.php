3671c27cd12cb48bc736711e6ab2b69127f0daf6
d8ahazard <d8ahazard@gmail.com>
2016-12-12 22:31:25 -0600

    Fix delete error
    Syntax...

--- muximux.php
+++ muximux.php
@@ -805,7 +805,7 @@ function deleteDir($dirPath) {
     $files = glob($dirPath . '*', GLOB_MARK);
     foreach ($files as $file) {
         if (is_dir($file)) {
-            self::deleteDir($file);
+            $this->deleteDir($file);
         } else {
             unlink($file);
         }
