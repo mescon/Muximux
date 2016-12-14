ac3294451c8d754c8f6540d8c1a938d549ce85ee
d8ahazard <d8ahazard@gmail.com>
2016-12-14 13:46:05 -0600

    Revert "Compress JS Using Closure Compiler Service"
    This reverts commit 69d4a4b1e42e5a2ee8ddb655f3610a479303ac5b.

--- js/login.js
+++ js/login.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
-  $(".wrapper").animate({top:"10%"}, 300, "easeInOutElastic", function() {
-    $(".logo").delay(1200).slideDown(400, "easeInOutElastic");
-    $(".login0").delay(1E3).slideDown(400, "easeInOutElastic");
-    $(".login1").delay(1100).slideDown(400, "easeInOutElastic");
-    $(".login2").delay(1200).slideDown(400, "easeInOutElastic");
-    $(".login3").delay(1300).slideDown(400, "easeInOutElastic");
-  });
+	$('.wrapper').animate({
+		top: "10%"
+	}, 300, 'easeInOutElastic', function() {
+		$('.logo').delay(1200).slideDown(400, 'easeInOutElastic');
+		$('.login0').delay(1000).slideDown(400, 'easeInOutElastic');
+		$('.login1').delay(1100).slideDown(400, 'easeInOutElastic');
+		$('.login2').delay(1200).slideDown(400, 'easeInOutElastic');
+		$('.login3').delay(1300).slideDown(400, 'easeInOutElastic');
+		// Animation complete.
+	});
 });
+
+
 function setBarColors() {
-  var a = rgb2hex($(".logo path").css("fill"));
-  $(".droidtheme").replaceWith('<meta name="theme-color" class="droidtheme" content="' + a + '" />');
-  $(".mstheme").replaceWith('<meta name="msapplication-navbutton-color" class="mstheme" content="' + a + '" />');
-  $(".iostheme").replaceWith('<meta name="apple-mobile-web-app-status-bar-style" class="iostheme" content="' + a + '" />');
+	var color = rgb2hex($('.logo path').css("fill"));
+	$('.droidtheme').replaceWith('<meta name="theme-color" class="droidtheme" content="' + color + '" />');
+	$('.mstheme').replaceWith('<meta name="msapplication-navbutton-color" class="mstheme" content="' + color + '" />');
+	$('.iostheme').replaceWith('<meta name="apple-mobile-web-app-status-bar-style" class="iostheme" content="' + color + '" />');
+}setTimeout(setBarColors, 1650);
+
+
+function rgb2hex(rgb) {
+     if (  rgb.search("rgb") == -1 ) {
+          return rgb;
+     } else {
+          rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+          function hex(x) {
+               return ("0" + parseInt(x).toString(16)).slice(-2);
+		}
+          return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]); 
+		}
 }
-setTimeout(setBarColors, 1650);
-function rgb2hex(a) {
-  if (-1 == a.search("rgb")) {
-    return a;
-  }
-  var b = function(a) {
-    return ("0" + parseInt(a).toString(16)).slice(-2);
-  };
-  a = a.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
-  return "#" + b(a[1]) + b(a[2]) + b(a[3]);
-}
-;
+
