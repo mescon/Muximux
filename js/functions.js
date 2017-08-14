f1135f2a0a7d6d69dbcb086bd71d5e8bee6447c3
digitalhigh <d8ahazard@gmail.com>
2017-08-14 09:15:49 -0500

    Fix Exploit
    We should not be trying to write anything to settings unless a valid
    secret is passed.
    
    This is just a band-aid, my guess is, every single interaction with the
    server-side should be re-reviewed and sanity-checked to make sure it's
    secure...

--- js/functions.js
+++ js/functions.js
@@ -234,9 +234,12 @@ function settingsEventHandlers() {
     var options = {
         url: 'muximux.php',
         type: 'post',
-        success: showResponse
+        success: showResponse,
+        secret: secret
     };
+
     $('#settingsSubmit').click(function(event) {
+        var form = $("form");
         event.preventDefault();
         $('.newApp').remove(); //Remove any new app that isn't filled out.
         $('.checkbox,.radio').each(function() {
@@ -246,7 +249,9 @@ function settingsEventHandlers() {
             }
         });
         $('.appName').removeAttr('disabled');
-        $("form").ajaxSubmit(options);
+        form.append('<input type="hidden" name="secret" value="'+secret+'" /> ');
+        form.append('<input type="hidden" name="function" value="write_ini" /> ');
+        form.ajaxSubmit(options);
     });
     $('#tabcolorCheckbox').click(function(event) {
         if ($(this).prop('checked')) {
@@ -557,6 +562,7 @@ function scaleFrames() {
 // Like the name implies, it changes the favicon to whatever
 // url is passed to it
 function changeFavicon(src) {
+    // noinspection JSAnnotator
     document.head = document.head || document.getElementsByTagName('head')[0];
     var link = document.createElement('link'),
         oldLink = document.getElementById('dynamic-favicon');
